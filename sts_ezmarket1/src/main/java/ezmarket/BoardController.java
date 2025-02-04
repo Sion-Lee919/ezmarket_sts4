473864dbcf41be79314a5383874cd3dc145e7d2a
minsu <minsur0919@gmail.com>
2025-02-04 11:45:25 +0900

    pom.xml 삭제 + board ->getItemDetail

--- sts_ezmarket1/src/main/java/ezmarket/BoardController.java
+++ sts_ezmarket1/src/main/java/ezmarket/BoardController.java
@@ -2,12 +2,26 @@ package ezmarket;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class BoardController {
 	
 	@Autowired
     @Qualifier("boardmapperservice")
     BoardService boardService;
+	
+	@CrossOrigin(origins = "http://localhost:3000")
+	@GetMapping("/item/{itemid}")
+	public BoardDTO GetItemDetail(@PathVariable("itemid") int product_id) {
+		System.out.println("hwre1==>"+product_id);
+		BoardDTO dto = boardService.getItemDetail(product_id);
+		System.out.println("hwre1==>"+dto);
+		return dto;
+		
+	}
+	
 }
