ddb064c23ba348b2c1c74a5e650e9942e54fee10
wendys1205 <wendys1205@naver.com>
2025-02-07 15:46:30 +0900

    CartController.java 수정

--- sts_ezmarket1/src/main/java/ezmarket/CartController.java
+++ sts_ezmarket1/src/main/java/ezmarket/CartController.java
@@ -1,13 +1,29 @@
 package ezmarket;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cart")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class CartController {
-	
-	@Autowired
-    @Qualifier("cartmapperservice")
-    CartService cartService;
+    private final CartService cartService;
+
+    @GetMapping("/{memberId}")
+    public List<CartDTO> getCart(@PathVariable Long memberId) {
+        return cartService.getCartByMemberId(memberId);
+    }
+
+    @PostMapping("/add")
+    public void addToCart(@RequestParam Long memberId, @RequestParam Long productId, @RequestParam int quantity) {
+        cartService.addToCart(memberId, productId, quantity);
+    }
+
+    @DeleteMapping("/remove/{cartId}")
+    public void removeFromCart(@PathVariable Long cartId) {
+        cartService.removeFromCart(cartId);
+    }
 }
