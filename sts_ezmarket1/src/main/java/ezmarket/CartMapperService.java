c462e194744acde5bd134701525acf543bb328c4
wendys1205 <wendys1205@naver.com>
2025-02-07 15:49:42 +0900

    CartMapperService.java 수정

--- sts_ezmarket1/src/main/java/ezmarket/CartMapperService.java
+++ sts_ezmarket1/src/main/java/ezmarket/CartMapperService.java
@@ -1,8 +1,24 @@
 package ezmarket;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service("cartmapperservice")
-public class CartMapperService implements CartService {
+import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+public class CartMapperService {
+    private final CartMapper cartMapper;
+
+    public List<CartDTO> getCartByMemberId(Long memberId) {
+        return cartMapper.getCartByMemberId(memberId);
+    }
+
+    public void addToCart(Long memberId, Long productId, int quantity) {
+        cartMapper.addToCart(memberId, productId, quantity);
+    }
+
+    public void removeFromCart(Long cartId) {
+        cartMapper.removeFromCart(cartId);
+    }
 }
