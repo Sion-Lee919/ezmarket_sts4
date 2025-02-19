f6f445772be8b8b6ce3226659c2e6b424c0d641e
minsu <minsur0919@gmail.com>
2025-02-19 17:51:37 +0900

    리뷰기능 개발중

--- sts_ezmarket1/src/main/java/ezmarket/ReviewMapper.java
+++ sts_ezmarket1/src/main/java/ezmarket/ReviewMapper.java
@@ -1,10 +1,14 @@
 package ezmarket;
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Mapper
 public interface ReviewMapper {
+
+	ArrayList<BoardDTO> getReview(int product_id);
 
 }
--- sts_ezmarket1/src/main/java/ezmarket/ReviewMapperService.java
+++ sts_ezmarket1/src/main/java/ezmarket/ReviewMapperService.java
@@ -1,8 +1,26 @@
 package ezmarket;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("reviewmapperservice")
 public class ReviewMapperService implements ReviewService {
+	
+	@Autowired
+	ReviewMapper mapper;
+
+	@Override
+	public ArrayList<BoardDTO> getReview(int product_id) {
+		// TODO Auto-generated method stub
+		return mapper.getReview(product_id);
+	}
+
+	@Override
+	public boolean registerReview(ReviewDTO dto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
