cd5c9ab6d21757541a9f592ff072d539da6be4c3
minsu <minsur0919@gmail.com>
2025-03-10 14:59:55 +0900

    상품 등록 개선

--- sts_ezmarket1/src/main/java/ezmarket/BoardController.java
+++ sts_ezmarket1/src/main/java/ezmarket/BoardController.java
@@ -90,6 +90,7 @@ public class BoardController {
 			file1.transferTo( new java.io.File(savePath +  newfilename1));
 			dto.setImage_url(newfilename1);
 		}
+		System.out.println(dto);
 		boolean result = boardService.registerItem(dto);
 	    return result ? ResponseEntity.ok(true) : ResponseEntity.status(HttpStatus.BAD_REQUEST).body(false);
 	}
